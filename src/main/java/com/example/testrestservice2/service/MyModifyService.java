cd818149edc6a231655000ad5f18b5affed7cd35
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2023-03-16 00:34:25 +0500

    done

---
+++ src/main/java/com/example/testrestservice2/service/MyModifyService.java
@@ -0,0 +1,7 @@
+package com.example.testrestservice2.service;
+
+import com.example.testrestservice2.model.Response;
+
+public interface MyModifyService {
+    Response modify(Response response);
+}
